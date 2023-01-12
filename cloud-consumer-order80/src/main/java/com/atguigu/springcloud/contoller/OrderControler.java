cb0a992b434dbfff728183267899c02f70f9291a
tangshuaiqi <tangshuaiqi@qq.com>
2023-01-12 10:33:57 +0800

    实现Eureka服务注册中心和服务提供商的集群功能（一定要记得打开RestTemplate的负载均衡）

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/contoller/OrderControler.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/contoller/OrderControler.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class OrderControler {
 
-	public static final String PAYMENT_URL = "http://localhost:8001";
+	public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
 	@Autowired
 	private RestTemplate restTemplate;
